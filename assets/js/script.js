b26404ff8c8e8366b9dc903d9638ab354195aa92
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-07-07 12:09:56 -0300

    primeiros passos selecionando e alterando elementos no dom

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const h1Title = window.document.querySelector("#title")
+const formLogin = window.document.querySelectorAll(".form-login")
+
+console.log(h1Title);
+console.log(formLogin);
+
+function bgChange() {
+    const imagem = document.querySelector("img");
+    imagem.style.display = "none";
+}
